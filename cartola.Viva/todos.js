cabd7e61b7be2d02584dd72170960278fa2e2638
tiagoarodrigues55 <tiago.americano.03@gmail.com>
2020-04-18 01:17:19 -0300

    removi o lixo e otimizei a seleção das informações
    Removi os arquivos que não estavam sendo usados e criei a função escolherConfronto( )

--- cartola.Viva/todos.js
+++ cartola.Viva/todos.js
@@ -6,7 +6,7 @@ var buttonElement = document.querySelector('#app button')
 var todos = JSON.parse(localStorage.getItem('list_todos')) || []
 var pre = JSON.parse(localStorage.getItem('list_preço')) || []
 
-
+escolherConfronto(Ottomanos, Time2)
 
 
 
@@ -51,15 +51,46 @@ var diego_g = {nome: 'Diego Garcia (dieguinho)', custo: 9, foto: 'none', time: '
 var otto_l = {nome: 'Otto Lautert (Ottera)', custo: 4, foto: 'otto_l.png', time: 'Ottomanos', media: 'função', ultimaPontuação: 'ultimo jogo', pontuação: 'próximo jogo', valorização: 'função'}
 var pedro_c = {nome: 'Pedro Caputo (Pedrão)', custo: 3, foto: 'pedro_c.png', time: 'Ottomanos', media: 'função', ultimaPontuação: 'ultimo jogo', pontuação: 'próximo jogo', valorização: 'função'}
 var tiago_r = {nome: 'Tiago Rodrigues (Americano)', custo: 8, foto: 'tiago_r.jpg', time: 'Ottomanos', media: 'função', ultimaPontuação: 'ultimo jogo', pontuação: 'próximo jogo', valorização: 'função'}
-var jogador7 = {nome: 'jogador 7', custo: 5, foto: 'none', time: 'outro', media: 'função', ultimaPontuação: 'ultimo jogo', pontuação: 'próximo jogo', valorização: 'função'}
+var jogador7 = {nome: 'Bento Rodrigues', custo: 15, foto: 'bl3.webp', time: 'outro', media: 'função', ultimaPontuação: 'ultimo jogo', pontuação: 'próximo jogo', valorização: 'função'}
+
+
 
 var fotoOttomanos = [antonio_t.foto, antonio_i.foto, diego_g.foto, otto_l.foto, pedro_c.foto, tiago_r.foto, jogador7.foto]
 var nomesOttomanos =[antonio_t.nome, antonio_i.nome, diego_g.nome, otto_l.nome, pedro_c.nome, tiago_r.nome, jogador7.nome]
 var preçosOttomanos = [antonio_t.custo, antonio_i.custo, diego_g.custo, otto_l.custo, pedro_c.custo, tiago_r.custo, jogador7.custo]
 
-var img = fotoOttomanos
-var jogadores = nomesOttomanos
-var preço = preçosOttomanos
+const Ottomanos = {
+    img: fotoOttomanos,
+    jogadores: nomesOttomanos,
+    preço: preçosOttomanos
+}
+const Time2 = {
+    img: ['foto1','foto2','foto3'],
+    jogadores: ['jogador1', 'jogador2', 'jogador3'],
+    preço: [1, 2, 3]
+}
+var a = [];
+var img = [];
+var jogadores = [];
+var preço = [];
+
+function escolherConfronto(nomeDoTime1, nomeDoTime2) {
+  a = nomeDoTime1.img;
+  a = nomeDoTime2.img.map(function (item) {
+    return a.push(item);
+  });
+  img = nomeDoTime1.img;
+  a = nomeDoTime1.jogadores;
+  a = nomeDoTime2.jogadores.map(function (item) {
+    return a.push(item);
+  });
+  jogadores = nomeDoTime1.jogadores;
+  a = nomeDoTime1.preço;
+  a = nomeDoTime2.preço.map(function (item) {
+    return a.push(item);
+  });
+  preço = nomeDoTime1.preço;
+}
 
 function addTodo(n) {
     var a = jogadores[n]
@@ -306,11 +337,11 @@ var textnome6 = document.createTextNode(`${jogadores[6]}`)
 nome6.appendChild(textnome6)
 div6.appendChild(nome6)
 
-/*var img6 = document.createElement('img')
+var img6 = document.createElement('img')
 img6.src = `${img[6]}`
 img6.width = 70
 img6.height = 90
-div6.appendChild(img6)*/
+div6.appendChild(img6)
 
 if (todos.indexOf(jogadores[6])!= -1){
     var botão6 = document.createElement('button')
